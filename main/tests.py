4beae95feeec9fe65c362c4092199d1ca4bd5f42
Student <student@example.com>
2026-03-05 03:32:26 +0500

    Add CI/CD workflow

--- main/tests.py
+++ main/tests.py
@@ -25,3 +25,26 @@ class ViewSmokeTests(TestCase):
     def test_products_list(self):
         resp = self.client.get(reverse('products'))
         self.assertEqual(resp.status_code, 200)
+
+from django.urls import reverse
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Category
+
+
+class AdditionalTests(TestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_creation(self):
+        user = User.objects.create_user(
+            username='testuser2',
+            password='testpass123'
+        )
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_category_string_representation(self):
+        category = Category.objects.create(name="Electronics")
+        self.assertEqual(str(category), "Electronics")
